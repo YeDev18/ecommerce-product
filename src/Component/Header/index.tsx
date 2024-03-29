eb8540b01a9467ef75fa330546c561c3eeb62da1
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-03-29 10:33:48 +0000

    Edit Header

---
+++ src/Component/Header/index.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <nav className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 ">
+      Header
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam
+        inventore iste consequuntur. Odio, dolorem iste. Repellat porro
+        reiciendis voluptate amet, nihil numquam, fuga cumque ratione non velit,
+        vero neque.
+      </p>
+    </nav>
+  );
+};
+
+export default Header;
