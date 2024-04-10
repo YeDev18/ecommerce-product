8f9d6c3910496f1afab200bc329640c0c93f678a
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-04-10 15:10:18 +0000

    Edit header

--- src/Component/Header/index.tsx
+++ src/Component/Header/index.tsx
@@ -5,36 +5,38 @@ import profil from '../../assets/images/image-avatar.png';
 
 const Header = () => {
   return (
-    <nav className="container max-w-auto mx-auto px-8 py-6 flex  justify-between items-center gap-y-8">
-      <div className="flex justify-between items-center gap-16 ">
-        <div className="flex justify-center items-center">
-          <img src={logo} alt="Logo Sneakers" />
+    <nav className="container max-w-auto mx-auto flex flex-col">
+      <div className="w-full px-8 py-6 flex justify-between">
+        <div className="flex justify-between items-center gap-16 ">
+          <div className="flex justify-center items-center">
+            <img src={logo} alt="Logo Sneakers" />
+          </div>
+
+          <ul className="flex font-sans font-[100] cursor-pointer text-lg text-DarkGrayishBlue gap-12 ">
+            {Headers.map((header: string, index: number) => {
+              return (
+                <li key={index} className="hover:text-VeryDarkBlue">
+                  {' '}
+                  {header}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-
-        <ul className="flex font-sans font-[100] cursor-pointer text-lg text-DarkGrayishBlue gap-12 ">
-          {Headers.map((header: string, index: number) => {
-            return (
-              <li key={index} className="hover:text-VeryDarkBlue">
-                {' '}
-                {header}
-              </li>
-            );
-          })}
-        </ul>
+        <div className="flex justify-center items-center gap-8 ">
+          <img
+            src={cart}
+            alt="Le panier"
+            className="w-6 hover:text-VeryDarkBlue cursor-pointer"
+          />
+          <img
+            src={profil}
+            alt="L'Avatar"
+            className="w-16 border-2 rounded-full p-1 border-firstPrimary"
+          />
+        </div>
       </div>
-
-      <div className="flex justify-center items-center gap-8 ">
-        <img
-          src={cart}
-          alt="Le panier"
-          className="w-6 hover:text-VeryDarkBlue cursor-pointer"
-        />
-        <img
-          src={profil}
-          alt="L'Avatar"
-          className="w-16 border-2 rounded-full p-1 border-firstPrimary"
-        />
-      </div>
+      <div className="border-b border-b-GrayishBlue mx-8"></div>
     </nav>
   );
 };
